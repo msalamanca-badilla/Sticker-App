518cb50f615c1c51ad144d000195e0619b9aec01
Marianne <m.salamanca21@live.ca>
2022-03-19 16:20:52 -0400

    add SignUpForm/LoginForm and AuthPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import './App.css';
-import Sticker from './components/SignUp/SignUp.jsx'
+import Sticker from './components/SignUpForm/SignUpForm.jsx'
 
 function App() {
+  state={
+    user:null,
+  }
+setUserInState=(incomingUserData)=>{
+  this.setState({user:incomingUserData})
+}
+
   return (
-    <div className="App">
-      <container>
-        <h1>Sticker</h1>
-        <Sticker />
-        </container>
-    </div>
+    // <div className="App">
+    //   <container>
+    //     <h1>Sticker</h1>
+    //     <Sticker />
+    //     </container>
+    // </div>
+
+<main className="App">
+{/* this ternary operator asks: is there a user in state? */}
+{/* if yes, they can see our pages: neworder, etc. */}
+{/* if no(user is null), show them only the <AuthPage> */}
+{ this.state.user ? 
+  <Switch>
+    <Route path='/orders/new' render={(props) => (
+      <NewOrderPage {...props}/>
+    )}/>
+    <Route path='/orders' render={(props) => (
+      <OrderHistoryPage {...props}/>
+    )}/>
+    <Redirect to="/orders" />
+  </Switch>
+  :
+  <AuthPage setUserInState={this.setUserInState}/>
+}
+</main>
   );
 }
 
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -10,11 +10,11 @@ function SignUp(props){
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" className="form-control" id="exampleInputPassword1"/>
             </div>
-            <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                <label className="form-check-label" for="exampleCheck1">Remember Me</label>
+            <div className="form-group">
+                <label for="exampleInputPassword1">Confirm Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"/>
             </div>
-            <button type="submit" className="btn btn-primary">Login</button>
+            <button type="submit" className="btn btn-primary">Sign Up</button>
         </form>
     )
 }
