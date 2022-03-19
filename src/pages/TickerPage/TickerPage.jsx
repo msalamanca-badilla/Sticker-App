09026eeb4de2935e757bf95475e4c63285bb5449
Marianne <m.salamanca21@live.ca>
2022-03-19 17:05:45 -0400

    add TickerPage

---
+++ src/pages/TickerPage/TickerPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TickerPage(props) {
+      return (
+        <main className="TickerPage">
+            <div>TickerPage</div>
+        </main>
+      );
+    }
+
+  export default TickerPage;
