566fda1f2ba687c8e71a0313a588a9911672fa6c
Marianne <m.salamanca21@live.ca>
2022-03-19 15:53:39 -0400

    add signup folder/file and render on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Sticker from './components/SignUp/SignUp.jsx'
 
 function App() {
   return (
     <div className="App">
+      <container>
         <h1>Sticker</h1>
+        <Sticker />
+        </container>
     </div>
   );
 }
