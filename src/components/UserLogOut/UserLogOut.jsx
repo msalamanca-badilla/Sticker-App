bd29de0baa62c19e2ed2edd1e713f6d6ddfd2dcf
Marianne <m.salamanca21@live.ca>
2022-03-19 20:03:55 -0400

    add UserLogout component

---
+++ src/components/UserLogOut/UserLogOut.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class UserLogout extends React.Component{
+    render(){
+        return(
+            <div className="UserLogOut">
+                <button className='btn-sm'></button>
+            </div>
+        )
+    }
+}
+
+export default UserLogout;
